f34fa2b02e51ddffa8e16059630fa25a8228d954
matheusmslima <matheusmslima@gmail.com>
2024-09-30 15:09:02 -0300

    added db queries for entries and transfers tables

---
+++ db/query/transfers.sql
@@ -0,0 +1,30 @@
+-- name: CreateTransfer :one
+INSERT INTO transfers (
+  from_account_id,
+  to_account_id,
+  amount
+) VALUES (
+  $1, $2, $3
+)
+RETURNING *;
+
+-- name: GetTransfer :one
+SELECT * FROM transfers
+WHERE id = $1 LIMIT 1;
+
+-- name: ListTransfers :many
+SELECT * FROM transfers
+WHERE from_account_id = $1 OR
+      to_account_id = $2
+ORDER BY id
+LIMIT $3
+OFFSET $4;
+
+
+CREATE TABLE "transfers" (
+  "id" bigserial PRIMARY KEY,
+  "from_account_id" bigint NOT NULL,
+  "to_account_id" bigint NOT NULL,
+  "amount" bigint NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
