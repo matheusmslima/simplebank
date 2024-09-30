c1718237dc9eb5a01f6b6b48f09c72f74711b495
matheusmslima <matheusmslima@gmail.com>
2024-09-30 19:23:16 -0300

    added unit tests for the account methods

--- db/query/transfers.sql
+++ db/query/transfers.sql
@@ -19,12 +19,3 @@ WHERE from_account_id = $1 OR
 ORDER BY id
 LIMIT $3
 OFFSET $4;
-
-
-CREATE TABLE "transfers" (
-  "id" bigserial PRIMARY KEY,
-  "from_account_id" bigint NOT NULL,
-  "to_account_id" bigint NOT NULL,
-  "amount" bigint NOT NULL,
-  "created_at" timestamptz NOT NULL DEFAULT (now())
-);
